0bb2378671f2f5a7c8487bce7e5c475e1727fabb
projectitis <peter@projectitis.com>
2022-05-07 19:52:50 +1200

    Auto-build assets

--- assets/build-assets.bat
+++ assets/build-assets.bat
@@ -2,7 +2,6 @@
 where python >nul 2>nul
 if not %ERRORLEVEL%==0 (
   echo Error: python not found. Please install python and ensure it is on the path
-)
-else (
-    python build-assets.py
+) else (
+    python %~dp0build-assets.py
 )
